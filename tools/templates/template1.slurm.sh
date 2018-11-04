490d7b46678d182919dd33c1c44dd8d64747d71e
cgorgulla <c.gorgulla@gmail.com>
2018-11-04 12:30:04 -0500

    Storing the SMILES in the PDB(QT) files

--- tools/templates/template1.slurm.sh
+++ tools/templates/template1.slurm.sh
@@ -20,7 +20,7 @@
 #SBATCH --mail-user=cgorgulla@crystal.harvard.edu
 #SBATCH --mail-type=fail
 #SBATCH --time=00-12:00:00
-#SBATCH --mem-per-cpu=5G
+#SBATCH --mem-per-cpu=1G
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
 #SBATCH --partition=main
