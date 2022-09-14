c8c456d96882af21f4c6e3e71fdef4f4cfdfa21c
Matt Koop <mkoop@amazon.com>
2022-09-14 01:28:45 +0000

    Improved cxcalc performance
    When using AWS Batch, this option uses a separate
    java class to get all properties at once instead of
    making multiple subprocess calls.

---
+++ tools/helper/vf/CxCalcAttr.java
@@ -0,0 +1,159 @@
+package vf;
+
+import chemaxon.marvin.calculations.*;
+import chemaxon.struc.*;
+import chemaxon.formats.MolImporter;
+import chemaxon.marvin.*;
+import chemaxon.marvin.plugin.*;
+import com.chemaxon.calculations.solubility.SolubilityCalculator;
+
+public class CxCalcAttr {
+
+	TopologyAnalyserPlugin pTAP;
+	int pTAPInit = 0;
+
+	HBDAPlugin pHBDA;
+	int pHBDAInit = 0;
+
+
+	private void initPTAP(Molecule mol) throws PluginException {
+		if (pTAPInit == 0) {
+			pTAP = new TopologyAnalyserPlugin();
+			pTAP.setMolecule(mol);
+			pTAP.run();
+			pTAPInit = 1;
+		}
+	}
+
+	private void initPHBDA(Molecule mol) throws PluginException {
+		if (pHBDAInit == 0) {
+			pHBDA = new HBDAPlugin();
+			pHBDA.setMolecule(mol);
+			pHBDA.run();
+			pHBDAInit = 1;
+		}
+	}
+
+
+    public static void main(String[] args) {
+		CxCalcAttr attr = new CxCalcAttr();
+
+		try {
+
+			MolImporter mi = new MolImporter(args[0]);
+			Molecule mol = mi.read();
+			mi.close();
+			
+			for (int i = 1; i < args.length; i++) {
+				String element = args[i];
+
+				switch (element.toLowerCase()) {
+					case "logp":
+
+						logPPlugin pLogP = new logPPlugin();
+						pLogP.setMolecule(mol);
+						pLogP.run();
+
+						System.out.println("logp," + pLogP.getlogPTrue());
+
+						break;
+					case "logd":
+
+						logDPlugin pLogD = new logDPlugin();
+						pLogD.setMolecule(mol);
+						pLogD.run();
+
+						System.out.println("logd," + pLogD.getlogD());
+						break;
+
+					case "logs":
+
+						SolubilityCalculator calculator = new SolubilityCalculator();
+						System.out.println("logs," + calculator.calculatePhDependentSolubility(mol,7.4).getSolubility());
+						break;
+
+					case "mass":
+
+						ElementalAnalyserPlugin pEAP = new ElementalAnalyserPlugin();
+						pEAP.setMolecule(mol);
+						pEAP.run();
+
+						System.out.println("mass," + pEAP.getMass());
+						break;
+
+					case "rotatablebondcount":
+						attr.initPTAP(mol);
+						System.out.println("rotatablebondcount," + attr.pTAP.getRotatableBondCount());
+						break;
+
+					case "bondcount":
+						attr.initPTAP(mol);
+						System.out.println("bondcount," + attr.pTAP.getBondCount());
+						break;
+
+					case "ringcount":
+						attr.initPTAP(mol);
+						System.out.println("ringcount," + attr.pTAP.getRingCount());
+						break;
+
+					case "aromaticringcount":
+						attr.initPTAP(mol);
+						System.out.println("aromaticringcount," + attr.pTAP.getAromaticRingCount());
+						break;
+
+					case "fsp3":
+						attr.initPTAP(mol);
+						System.out.println("fsp3," + attr.pTAP.getFsp3());
+						break;
+
+					case "chiralcentercount":
+						attr.initPTAP(mol);
+						System.out.println("chiralcentercount," + attr.pTAP.getChiralCenterCount());
+						break;
+
+					case "refractivity":
+						RefractivityPlugin pRefractivity = new RefractivityPlugin();
+						pRefractivity.setMolecule(mol);
+						pRefractivity.run();
+
+						System.out.println("refractivity," + pRefractivity.getRefractivity());
+						break;
+
+					case "polarsurfacearea":
+						TPSAPlugin pTPSA = new TPSAPlugin();
+						pTPSA.setMolecule(mol);
+						pTPSA.run();
+
+						System.out.println("polarsurfacearea," + pTPSA.getSurfaceArea());
+						break;
+
+					case "donorcount":
+						attr.initPHBDA(mol);
+						System.out.println("donorcount," + attr.pHBDA.getDonorSiteCount());
+						break;
+
+					case "acceptorcount":
+						attr.initPHBDA(mol);
+						System.out.println("acceptorcount," + attr.pHBDA.getAcceptorSiteCount());
+						break;
+
+					case "atomcount":
+						attr.initPHBDA(mol);
+						System.out.println("atomcount," + attr.pHBDA.getAtomCount());
+						break;
+
+					default:
+						System.out.println(element.toLowerCase() + ",INVALID");
+						break;
+				}
+			}
+
+		} catch(Exception e) {
+        	System.out.println("ERROR:" + e); 
+		}
+
+
+
+
+    }
+}
